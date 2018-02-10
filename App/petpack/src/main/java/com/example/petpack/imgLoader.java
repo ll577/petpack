b04714dc5c7c7e0cd74c5ca0a62aba474778e43f
obsolesce <28615122+obsolesce@users.noreply.github.com>
2018-02-09 20:43:43 -0600

    display the image
    image displays

--- App/petpack/src/main/java/com/example/petpack/imgLoader.java
+++ App/petpack/src/main/java/com/example/petpack/imgLoader.java
@@ -1,13 +1,19 @@
 package com.example.petpack;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class imgLoader extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMAGE = 217;
     Bitmap petPhoto;
 
     @Override
@@ -16,19 +22,24 @@ public class imgLoader extends AppCompatActivity {
         setContentView(R.layout.activity_img_loader);
         //called once on load, shoudl probably change this but whatevz
         pickImage();
+        ImageView mImg;
+        mImg = findViewById(R.id.background);
+        mImg.setImageBitmap(petPhoto);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (resultCode != RESULT_OK) {
             return;
         }
-        if (requestCode == 1) {
-            final Bundle extras = data.getExtras();
-            if (extras != null) {
-                //Get image
-                petPhoto = extras.getParcelable("data");
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
+                //Get image set as imgview
+                ImageView mImg;
+                mImg = (ImageView) findViewById(R.id.background);
+                Uri imageUri = data.getData();
+                mImg.setImageURI(imageUri);
                 //now we have a photo to feed wherever
-            }
         }
     }
     public void pickImage() {
@@ -36,14 +47,8 @@ public class imgLoader extends AppCompatActivity {
                 MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         //data to pass to gallery to return a photo with X parameters
         intent.setType("image/*");
-        intent.putExtra("crop", "true");
-        intent.putExtra("scale", true);
-        intent.putExtra("outputX", 1000);
-        intent.putExtra("outputY", 1000);
-        intent.putExtra("aspectX", 1);
-        intent.putExtra("aspectY", 1);
         intent.putExtra("return-data", true);
         //open gallery
-        startActivityForResult(intent, 1);
+        startActivityForResult(intent, RESULT_LOAD_IMAGE);
     }
 }
