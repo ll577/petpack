d789ade77dcbb825af68df61949ca351d7303015
obsolesce <28615122+obsolesce@users.noreply.github.com>
2018-02-11 22:44:08 -0600

    now it can change colors based on the picture

--- App/petpack/src/main/java/com/example/petpack/imgLoader.java
+++ App/petpack/src/main/java/com/example/petpack/imgLoader.java
@@ -1,30 +1,39 @@
 package com.example.petpack;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 
-public class imgLoader extends AppCompatActivity {
+import org.michaelevans.colorart.library.ColorArt;
 
+import java.io.IOException;
+
+public class imgLoader extends AppCompatActivity {
+    //this static var is letting us pass various 'intents' to the on activity result listener
+    //well use switch statements so we only need one override function
+    //this is just a random number, it only needs to be unique and a constant
+    //named for clarity
     private static final int RESULT_LOAD_IMAGE = 217;
     Bitmap petPhoto;
+    ColorArt primaryColors;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_img_loader);
         //called once on load, shoudl probably change this but whatevz
+        //TODO move this to a button or something and design a layout
         pickImage();
-        ImageView mImg;
-        mImg = findViewById(R.id.background);
-        mImg.setImageBitmap(petPhoto);
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -34,14 +43,42 @@ public class imgLoader extends AppCompatActivity {
             return;
         }
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
-                //Get image set as imgview
-                ImageView mImg;
-                mImg = (ImageView) findViewById(R.id.background);
-                Uri imageUri = data.getData();
-                mImg.setImageURI(imageUri);
-                //now we have a photo to feed wherever
+               loadImage(data);
+
         }
     }
+    public void loadImage(Intent data) {
+        //Get image set as imgview
+        Uri imageUri = data.getData();
+
+
+        try {
+            petPhoto=MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ImageView mImg = findViewById(R.id.loadedPic);
+        Drawable drawable = new BitmapDrawable(getResources(),petPhoto);
+        mImg.setImageDrawable(drawable);
+        //now we have a photo to feed wherever
+
+        //extract the colors
+
+        // get the colors
+        //https://github.com/MichaelEvans/ColorArt
+//            colorArt.getBackgroundColor()
+//            colorArt.getPrimaryColor()
+//            colorArt.getSecondaryColor()
+//            colorArt.getDetailColor()
+
+
+        //TODO  //if bitmap is too large color picking crashes
+        if (petPhoto != null) {
+            primaryColors = new ColorArt(petPhoto);
+            findViewById(R.id.backgroundColor).setBackgroundColor(primaryColors.getSecondaryColor());
+        }
+    }
+
     public void pickImage() {
         Intent intent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.INTERNAL_CONTENT_URI);
@@ -51,4 +88,5 @@ public class imgLoader extends AppCompatActivity {
         //open gallery
         startActivityForResult(intent, RESULT_LOAD_IMAGE);
     }
+
 }
