4c665ccce699277eb4281d02a2c98ccb3d28cc25
obsolesce <28615122+obsolesce@users.noreply.github.com>
2018-03-13 21:52:13 -0500

    more better looking

--- App/petpack/src/main/java/com/example/petpack/mainMenu.java
+++ App/petpack/src/main/java/com/example/petpack/mainMenu.java
@@ -1,17 +1,25 @@
 package com.example.petpack;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
 import android.widget.Button;
 
 public class mainMenu extends Activity {
-
+    ConstraintLayout container;
+    AnimationDrawable anim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        container = findViewById(R.id.mainMenuBG);
+        anim = (AnimationDrawable) container.getBackground();
+        anim.setEnterFadeDuration(2500);
+        anim.setExitFadeDuration(2500);
 
         //main menu button for jumping to image loader
         final Button button = findViewById(R.id.gotoImageLoader);
@@ -23,21 +31,25 @@ public class mainMenu extends Activity {
 
             }
         });
-        final Button button1 = findViewById(R.id.gotoCreatePet);
-        button1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), createPet.class);
-                startActivity(intent);
-
-            }
-        });
 
 
 
 
 
 
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (anim != null && !anim.isRunning())
+            anim.start();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (anim != null && anim.isRunning())
+            anim.stop();
     }
 
 }
